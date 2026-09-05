b89b590934d0a31f47ef02c1f2f55bf9b9ae0dc2
Emanuel <emanuelgama219@gmail.com>
2026-09-04 22:24:27 -0400

    add project

---
+++ src/main/java/model/Pessoa.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Pessoa {
+    private String nome;
+    private Cidade cidade;
+    private String email;
+    private String fone;
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
